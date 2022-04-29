9cbbcd91684607c2d05cdef4a0db441b2245d812
Aishani <aishani1@umd.edu>
2022-04-29 18:27:16 -0400

    worked on displaying story

--- app/src/main/java/com/example/readaloudwithstuffedanimal/ReadStoryContentActivity.kt
+++ app/src/main/java/com/example/readaloudwithstuffedanimal/ReadStoryContentActivity.kt
@@ -4,14 +4,22 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 
 class ReadStoryContentActivity : AppCompatActivity(),TextToSpeech.OnInitListener {
     private var textToSpeech: TextToSpeech? = null
     private var readText: String? = null
+    private lateinit var btnNext: ImageButton
+    private lateinit var btnPrevious: ImageButton
+    private lateinit var textPageNumber: TextView
+    private lateinit var storyContent: TextView
+    private var content: String? = null
+    private var pageNumber: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +31,33 @@ class ReadStoryContentActivity : AppCompatActivity(),TextToSpeech.OnInitListener
         // Set status bar color.
         window.statusBarColor = ContextCompat.getColor(this, R.color.black)
 
-        val btnNext: ImageButton = findViewById(R.id.btn_next)
+        btnNext = findViewById(R.id.btn_next)
+        btnPrevious = findViewById(R.id.btn_previous)
+        textPageNumber = findViewById(R.id.text_page_number)
         val storyTitle: TextView = findViewById(R.id.text_story_title)
-        val storyContent: TextView = findViewById(R.id.text_story_content)
+        storyContent = findViewById(R.id.text_story_content)
 
         textToSpeech = TextToSpeech(this, this)
         val id = intent.getStringExtra(StoryContentActivity.STORY_ID)
         val sp = getSharedPreferences(id, Context.MODE_PRIVATE)
         val title = sp.getString(StoryTitleActivity.STORY_TITLE, null)
-        val content = sp.getString(StoryContentActivity.STORY_CONTENT, null)
+        content = sp.getString(StoryContentActivity.STORY_CONTENT, null)
         readText = content
 
         storyTitle.text = title
-        storyContent.text = content
+        //storyContent.text = content
+
+        updatePage(true)
+
+        btnNext.setOnClickListener {
+            storyContent.text = content
+            updatePage(true)
+        }
+
+        btnPrevious.setOnClickListener {
+            storyContent.text = content
+            updatePage(false)
+        }
     }
 
     private fun read(text: String?) {
@@ -45,6 +67,24 @@ class ReadStoryContentActivity : AppCompatActivity(),TextToSpeech.OnInitListener
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
             read(readText)
+        }
+    }
+
+    private fun updatePage(nextPage: Boolean) {
+        if (nextPage) {
+            pageNumber++
+        } else {
+            pageNumber--
+        }
+
+        textPageNumber.text = pageNumber.toString()
+
+        storyContent.text = content
+
+        if (pageNumber == StoryContentActivity.FIRST_PAGE) {
+            btnPrevious.visibility = View.GONE
+        } else {
+            btnPrevious.visibility = View.VISIBLE
         }
     }
 }
