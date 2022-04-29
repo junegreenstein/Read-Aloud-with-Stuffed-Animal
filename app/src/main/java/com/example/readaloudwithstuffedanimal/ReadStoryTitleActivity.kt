1df400b1c6d78ff33081adf26d47c87ede1b85dd
Aishani <aishani1@umd.edu>
2022-04-28 22:18:14 -0400

    update

--- app/src/main/java/com/example/readaloudwithstuffedanimal/ReadStoryTitleActivity.kt
+++ app/src/main/java/com/example/readaloudwithstuffedanimal/ReadStoryTitleActivity.kt
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.widget.EditText
+import android.widget.ImageButton
 import java.io.File
+import android.widget.TextView
+import android.content.Intent
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class ReadStoryTitleActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var textToSpeech: TextToSpeech? = null
@@ -15,12 +20,24 @@ class ReadStoryTitleActivity : AppCompatActivity(), TextToSpeech.OnInitListener 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_read_story_title)
 
+        val btnNext: ImageButton = findViewById(R.id.btn_next)
+        val StoryTitle: TextView = findViewById(R.id.read_story_title)
+        val AuthorName: TextView  = findViewById(R.id.read_author_name)
+
        textToSpeech = TextToSpeech(this, this)
         val id = intent.getStringExtra(StoryContentActivity.STORY_ID)
         val sp = getSharedPreferences(id, Context.MODE_PRIVATE)
         val title = sp.getString(StoryTitleActivity.STORY_TITLE, null)
         val author = sp.getString(StoryTitleActivity.AUTHOR_NAME, null)
         readText = title + author
+
+        StoryTitle.text = title
+        AuthorName.text = author
+
+        btnNext.setOnClickListener {
+            val intent = Intent(this, ReadStoryContentActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun read(text: String?) {
